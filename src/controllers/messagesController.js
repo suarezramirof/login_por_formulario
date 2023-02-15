bbaa3811a4e59c62bc588e1d4d26e29818face40
Ramiro Suarez <suarezramirof@gmail.com>
2023-02-14 22:41:52 -0300

    EDIT controller names

---
+++ src/controllers/messagesController.js
@@ -0,0 +1,34 @@
+import { messages } from "../daos/index.js";
+import { normalize, schema } from "normalizr";
+import keys from "../ws_keys.js";
+const user = new schema.Entity("author");
+const schemaMessages = new schema.Entity("messages", { author: user });
+class MessagesController {
+  constructor(messages) {
+    this.messages = messages;
+  }
+
+  normalizeData = (data) => {
+    return normalize(data, [schemaMessages]);
+  };
+
+  addMessage = async (msj, io) => {
+    msj.author.avatar = msj.author.avatar.startsWith("http")
+      ? msj.author.avatar
+      : "https://castillotrans.eu/wp-content/uploads/2019/06/77461806-icono-de-usuario-hombre-hombre-avatar-avatar-pictograma-pictograma-vector-ilustraci%C3%B3n-300x300.jpg";
+    await this.messages.add(msj);
+    const messages = await this.messages.getAll();
+    const normalizedMessages = this.normalizeData(messages);
+    io.sockets.emit(keys.nuevoMensaje, normalizedMessages);
+  };
+
+  viewMessages = async (socket) => {
+    const messages = await this.messages.getAll();
+    const normalizedMessages = this.normalizeData(messages);
+    socket.emit(keys.nuevoMensaje, normalizedMessages);
+  };
+}
+
+const messagesController = new MessagesController(messages);
+
+export default messagesController;
---
+++ src/controllers/productsController.js
@@ -0,0 +1,37 @@
+import { products } from "../daos/index.js";
+import keys from "../ws_keys.js";
+
+class ProductsController {
+  constructor(products) {
+    this.products = products;
+  }
+
+  getProducts = (req, res) => {
+    if (req.session.user) {
+      this.products
+        .getAll()
+        .then((data) => res.json(data))
+        .catch((error) => res.json(error));
+    } else {
+      res.status(401).json({ error: "No tiene permisos" });
+    }
+  };
+
+  addProduct = (req, res) => {
+    this.products
+      .add(req.body)
+      .then(() => res.json("Éxito"))
+      .catch(() => res.send("Error"));
+  };
+
+  addProductSocket = (prod, io) => {
+    this.products
+      .add(prod)
+      .then(() => io.sockets.emit(keys.nuevoProducto))
+      .catch(() => console.log(error));
+  };
+}
+
+const productsController = new ProductsController(products);
+
+export default productsController;
